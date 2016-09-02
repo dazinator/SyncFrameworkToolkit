53506a3fb0a9e1de0a9f0220f2f3a4e10bbeb5f9
dazinator <darrell.tunnell@googlemail.com>
2016-09-02 18:43:40 +0100

    Added experimental host configured to sync a couple of entities

---
+++ src/Sync.Host/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using SyncTest;
+
+namespace Sync.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var host = new ServiceHost(typeof(TestScopeSyncService)))
+            {
+                host.Open();
+                Console.WriteLine("ServiceHost started");
+                Console.ReadLine();
+                host.Close();
+            }
+
+        }
+    }
+}
